f829059e339c8d1b6e7264b3a50ceb42ba625639
Johnny <Johnny.savu.99@gmail.com>
2020-05-08 13:52:01 +0300

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,11 @@ document.getElementById("home-btn").addEventListener("click", goHome);
 document.getElementById("log-in-btn").addEventListener("click", goLogIn);
 document.getElementById("search-cars-btn").addEventListener("click", goSearchCars);
 document.getElementById("add-cars-btn").addEventListener("click", goAddCars);
+localStorage.setItem("onSearch", "false");
+
 
 function goLogIn(){
+    localStorage.setItem("onSearch", "false");
     main = document.getElementById('main-section');
     main.innerHTML = `<main class = 'main-div-sign'>
     <form>
@@ -41,7 +44,7 @@ function goLogIn(){
 }
 
 function goAddCars(){
-
+    localStorage.setItem("onSearch", "false");
     main = document.getElementById('main-section');
     main.innerHTML =`<main class = 'main-div'>
     <div class='add-car'>
@@ -67,13 +70,13 @@ function goAddCars(){
                Add description:
            </h3>
            <form>
-            <textarea name="message" class='add-input-desc' rows="20" cols="150"></textarea>
+            <textarea name="message" id = 'car-desc' class='add-input-desc' rows="20" cols="150"></textarea>
            </form>
            <br>
 
         </div>
         
-        <button type='button' class='add-car-btn'>
+        <button type='button' class='add-car-btn' id = 'add-car-btn'>
             Add the car!
         </button>
     </div>
@@ -83,8 +86,89 @@ function goAddCars(){
 }
 
 function goSearchCars(){
-    main = document.getElementById('main-section');
-    main.innerHTML = `<main class = 'main-div-search'>
+    
+    //if we try to reload the list but there is no need to, abort
+    if(localStorage.getItem("onSearch") == 'true')
+    {
+        console.log(localStorage.getItem("onSearch"));
+        return;
+    }
+    localStorage.setItem("onSearch",'true');
+    let mainSection = document.getElementById('main-section');
+    while(mainSection.firstChild)
+    {
+        mainSection.removeChild(mainSection.firstChild);
+    }
+    let main_div = document.createElement("main");
+    main_div.className = "main-div-search";
+    main_div.id = "main-div-search";
+    
+    //let main = document.getElementById('main-div-search');
+    //while(main.firstChild)
+        //main.removeChild(main.firstChild);
+    fetch('http://localhost:3000/cars')
+    .then(function (response) {
+        // Trasform server response to get the dogs
+        response.json().then(function (cars) {
+            for(let i = 0; i < cars.length; i++)
+            {
+                let div_id = document.createElement("div");
+                div_id.id = cars[i].id;
+                div_id.className = 'car';
+
+                let div_poz_titlu = document.createElement("div");
+                div_poz_titlu.className = 'poza-titlu';
+                
+                let div_desc = document.createElement("div");
+                div_desc.className = 'desc-produs';
+                
+                let img = document.createElement("img");
+                img.className = 'img';
+                img.alt = 'Car'
+                img.src = cars[i].img;
+                
+                div_desc.appendChild(img);
+                
+                let div_titlu_pret = document.createElement("div");
+                div_titlu_pret.className = "titlu-pret";
+
+                let h3 = document.createElement("p");
+                h3.className = 'titlu';
+                h3.innerText = cars[i].name;
+
+                let h4 = document.createElement("p");
+                h4.className = 'pret';
+                h4.innerText = cars[i].price;
+
+                div_titlu_pret.appendChild(h3);
+                div_titlu_pret.appendChild(h4);
+                div_poz_titlu.appendChild(div_desc);
+                div_poz_titlu.appendChild(div_titlu_pret);
+
+                let div_descriere = document.createElement("div");
+                div_descriere.className = "descriere";
+                div_descriere.innerText = "Description: " + cars[i].description;
+
+                let btn = document.createElement("button");
+                btn.className = "btn-buy";
+                btn.type = "button";
+                btn.innerText = "Buy it!";
+
+                div_id.appendChild(div_poz_titlu);
+                div_id.appendChild(div_descriere);
+                div_id.appendChild(btn);
+
+                main_div.appendChild(div_id);
+
+            }
+            
+        });
+    });
+
+    mainSection.appendChild(main_div);
+/*
+
+    mainSection.innerHTML = `<main class = 'main-div-search' id = 'main-div-search'>
     <div id = '1' class='car'>
                 <div class = 'poza-titlu'>
                     <div class ='desc-produs'>
@@ -107,82 +191,15 @@ function goSearchCars(){
                 <button type='button' class='btn-buy'>
                     Buy it!
                 </button>
-            </div>
-            <div id = '1' class='car'>
-                <div class = 'poza-titlu'>
-                    <div class ='desc-produs'>
-                        <img class = 'img' alt='Car'  src='./img/car1.jpg'>
-                    </div>
-                    <div class = 'tilu-pret'>
-                        <h3 class='titlu'>
-                            BMW 320d
-                        </h3>
-                        <h4 class='pret'>
-                            3.200$
-                        </h4>
-                    </div>
-                </div>
+    </div>
 
-                <div class = 'descriere'>
-                   Descriere: lorem ipsum
-                </div>
-                
-                <button type='button' class='btn-buy'>
-                    Buy it!
-                </button>
-            </div>
-            <div id = '1' class='car'>
-                <div class = 'poza-titlu'>
-                    <div class ='desc-produs'>
-                        <img class = 'img' alt='Car'  src='./img/car1.jpg'>
-                    </div>
-                    <div class = 'tilu-pret'>
-                        <h3 class='titlu'>
-                            BMW 320d
-                        </h3>
-                        <h4 class='pret'>
-                            3.200$
-                        </h4>
-                    </div>
-                </div>
-
-                <div class = 'descriere'>
-                   Descriere: lorem ipsum
-                </div>
-                
-                <button type='button' class='btn-buy'>
-                    Buy it!
-                </button>
-            </div>
-            <div id = '1' class='car'>
-                <div class = 'poza-titlu'>
-                    <div class ='desc-produs'>
-                        <img class = 'img' alt='Car'  src='./img/car1.jpg'>
-                    </div>
-                    <div class = 'tilu-pret'>
-                        <h3 class='titlu'>
-                            BMW 320d
-                        </h3>
-                        <h4 class='pret'>
-                            3.200$
-                        </h4>
-                    </div>
-                </div>
-
-                <div class = 'descriere'>
-                   Descriere: lorem ipsum
-                </div>
-                
-                <button type='button' class='btn-buy'>
-                    Buy it!
-                </button>
-            </div>
-    
 
 </main>`;
+*/
 }
 
 function goHome(){
+    localStorage.setItem("onSearch", "false");
     main = document.getElementById('main-section');
     main.innerHTML = `<main class = 'main-div'>      
 </main>`;
---
+++ assets/test.js
@@ -0,0 +1,64 @@
+
+
+//post a car in the list!
+
+document.addEventListener('click',function(e)
+{
+    if(e.target && e.target.id == 'add-car-btn')
+    {
+        carName = document.getElementById("car_name").value;
+        price = document.getElementById("price").value;
+        imgLink = document.getElementById("Image").value;
+        carDesc = document.getElementById("car-desc").value;
+        obj = {
+            name: carName,
+            price: price,
+            description: carDesc,
+            img: imgLink
+        }
+        postCar(obj);
+    }
+});
+
+function postCar(postObject)
+{
+
+    fetch('http://localhost:3000/cars', {
+        method: 'post',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(postObject)
+    }).then(function () {
+        document.getElementById("car_name").value = "";
+        document.getElementById("price").value = "";
+        document.getElementById("Image").value = "";
+        document.getElementById("car-desc").value = "";
+        //de adaugat move to search cars 
+    });
+
+}
+
+
+
+
+
+/*
+
+var addCarBtn = null;
+if (document.getElementById('main-div'))
+{
+    console.log("binie");
+    addCarBtn = document.getElementById('main-div');
+    addCarBtn.addEventListener('click', works);
+}
+else
+{
+    addCarBtn = null;
+}
+
+function works()
+{
+    console.log("binie");
+}
+*/
